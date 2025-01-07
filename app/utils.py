74345096e52ecc0b6416c5dd619db39c4c29df47
Cher-Cash <119042176+Cher-Cash@users.noreply.github.com>
2025-01-07 18:46:00 +1000

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -4,10 +4,10 @@ import os
 
 
 def generate_signature(params):
-    hash_object = hashlib.sha256()
-    for key in ['status', 'company_id', 'order_id']:
-        hash_object.update(params.get(key).encode())
-        print(hash_object.hexdigest(), params.get(key))
+    order_id = params.get('order_id')
+    company_id = params.get('company_id')
+    secret_key = params.get('secret_key')
+    sigma = f"{order_id}{company_id}{secret_key}"
+    hash_object = hashlib.sha256(sigma.encode())
     signature = hash_object.hexdigest()
     return signature
-
